4751af59d698d4c6a8f41806134ea1f0453cbea2
Ilyas <128220841+ilyas-asmouki@users.noreply.github.com>
2024-09-22 22:01:50 +0200

    fixed weird edge cases

--- src/terminal_input/src/terminal_command_interface.cpp
+++ src/terminal_input/src/terminal_command_interface.cpp
@@ -25,7 +25,7 @@ void TerminalCommandInterface::listen()
 
         while (iss >> motor) 
         {
-            if (!(iss >> velocity)) {
+            if (!(iss >> velocity) and motor != "st") {
                 std::cout << "Missing velocity for " << motor << std::endl;
                 invalid = true;
                 break;
@@ -42,9 +42,13 @@ void TerminalCommandInterface::listen()
             } else if (motor == "fw") {  // forward: set both front motors to the same velocity
                 fr_velocity = velocity;
                 fl_velocity = velocity;
+                rr_velocity = velocity;
+                rl_velocity = velocity;
             } else if (motor == "bw") {  // backward: set both front motors to the same negative velocity
                 fr_velocity = -velocity;
                 fl_velocity = -velocity;
+                rr_velocity = -velocity;
+                rl_velocity = -velocity;
             } else if (motor == "rt") {  // rotate: set front and rear motors to opposite velocities for rotation
                 fr_velocity = velocity;
                 fl_velocity = -velocity;
@@ -66,10 +70,9 @@ void TerminalCommandInterface::listen()
         }
 
         if (!invalid) {
-            sendMotorCommand("fr", fr_velocity);
-            sendMotorCommand("fl", fl_velocity);
-            sendMotorCommand("rr", rr_velocity);
-            sendMotorCommand("rl", rl_velocity);
+            std::ostringstream oss;
+            oss << "fr " << fr_velocity << " fl " << fl_velocity << " rr " << rr_velocity << " rl " << rl_velocity;
+            sendMotorCommand(oss.str());
         }
 
         std::cout << "Enter commands: " << std::endl;
@@ -77,14 +80,12 @@ void TerminalCommandInterface::listen()
 }
 
 // Helper function to send motor commands to ROS
-void TerminalCommandInterface::sendMotorCommand(const std::string &motor, int velocity)
+void TerminalCommandInterface::sendMotorCommand(const std::string &command)
 {
     auto message = motor_controller::msg::MotorCommand();
-    message.motor = motor;
-    message.velocity = velocity;
+    message.motor = command;  // Use the entire command string
     command_publisher->publish(message);
 }
-
 
 int main(int argc, char* argv[])
 {
