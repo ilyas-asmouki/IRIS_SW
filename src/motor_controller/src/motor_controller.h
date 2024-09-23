3106005dd8422ddf254b28d3243578b467427ca0
Ilyas <128220841+ilyas-asmouki@users.noreply.github.com>
2024-09-24 01:25:15 +0200

    keyboard listener fully functional now

--- src/motor_controller/src/motor_controller.h
+++ src/motor_controller/src/motor_controller.h
@@ -3,19 +3,26 @@
 #include <iostream>
 #include <string>
 #include "motor_controller/msg/motor_command.hpp"
+#include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
 class MotorController : public rclcpp::Node 
 {
-    public:
-        MotorController();
-        int readData();
-        void sendData(std::string data);
+public:
+    MotorController();
+    int read_data();
+    void send_data(std::string data);
 
-    private:
-        void communicate();
-        serial::Serial serial;
-        rclcpp::Subscription<motor_controller::msg::MotorCommand>::SharedPtr subscription;
-        void commandCallback(const motor_controller::msg::MotorCommand::SharedPtr msg);
+private:
+    // void communicate();
+    int fr_velocity;
+    int fl_velocity;
+    int rr_velocity;
+    int rl_velocity;
+    serial::Serial serial;
+    rclcpp::Subscription<motor_controller::msg::MotorCommand>::SharedPtr terminal_subscription;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr keyboard_subscription;
+    void terminal_callback(const motor_controller::msg::MotorCommand::SharedPtr msg);
+    void keyboard_callback(const std_msgs::msg::String::SharedPtr msg);
 };
